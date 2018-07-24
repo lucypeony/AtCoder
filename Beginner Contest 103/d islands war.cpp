4b63d6bec675ab632c09030253256cd227309074
lucypeony <lucypeony@hotmail.com>
2018-07-24 20:41:33 +0800

    贪心

--- Beginner Contest 103/d islands war.cpp
+++ Beginner Contest 103/d islands war.cpp
@@ -1,5 +1,11 @@
 /*
+给你n个点，他们1到n依次从左到右连接。问你m对数(A,B)。AB不能相连。
+问你最少要拆多少条边.
+
 abc103_d
+
+先按右边端点进行排序，先选择右边端点最小的。如果别的点的左边大于现在被割掉的桥的右边，则新的被断掉的桥就是新的这个点
+贪心法
 */
 #include<algorithm>
 #include<iostream>
@@ -8,25 +14,38 @@ abc103_d
 #include<string>
 using namespace std;
 
-struct p {
-	int x;
-	int y;
-};
+typedef long long ll;
+struct node {
+	int l, r;
+}a[100000+20];
+
+bool cmp(node a, node b) {
+	if (a.r == b.r)
+		return a.l < b.l; //右边相同比较左边
+	return a.r < b.r;		//对数据右进行排序
+}
+
 int main() {
 	freopen("d.in", "r", stdin);
 	freopen("d.out", "w", stdout);
-	int N, M;
-	vector<p> pairs;
-	int x, y;
-	for (int i = 0; i < M; i++)
-	{
-		cin >> x >> y;
-		p myp;
-		myp.x = x;
-		myp.y = y;
-		pairs.push_back(myp);
-	}
+	int n, m, i;
+	cin >> n >> m;
+	for (i = 0; i < m; i++)
+		cin >> a[i].l >> a[i].r;
+	int mn = a[0].r;
+	for (i = 0; i < m; i++)
+		mn = min(a[i].r, mn);
 
+	sort(a, a + m, cmp);
+
+	int now = mn, ans = 1;  //find the smallest 
+	for (i = 1; i < m; i++) {
+		if (a[i].l >= now) {
+			now = a[i].r;//如果下一个数据的左边大于切断桥的右边则仍然需要切桥
+			ans++;
+		}
+	}
+	cout << ans << endl;
 
 	fclose(stdin);
 	fclose(stdout);
